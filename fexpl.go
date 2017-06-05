4cd9e0f8bd5bdf9fcf8017f4ebde64a73fb5673e
Gildas Chabot <gildas.chabot@scmfrance.fr>
2017-06-05 20:18:51 +0200

    Extract type from file header

--- fexpl.go
+++ fexpl.go
@@ -6,6 +6,9 @@ import (
 	"strings"
 	"time"
 
+	filetype "gopkg.in/h2non/filetype.v1"
+	filetypes "gopkg.in/h2non/filetype.v1/types"
+
 	"github.com/GildasCh/fcomp"
 )
 
@@ -14,6 +17,7 @@ type File struct {
 	Hash     string
 	Size     int64
 	Modified time.Time
+	Header   filetypes.Type
 }
 
 type FileCollection struct {
@@ -25,13 +29,15 @@ func Explore(name, root string) FileCollection {
 	ret := FileCollection{}
 	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		file, _ := os.Open(path)
-		hash := fcomp.Hash(file)
-
+		kh := &KeepHeaders{r: file}
+		hash := fcomp.Hash(kh)
+		ft, _ := filetype.Get(kh.headers[:])
 		ret.Files = append(ret.Files, File{
 			Name:     strings.TrimPrefix(path, root),
 			Hash:     hash,
 			Size:     info.Size(),
 			Modified: info.ModTime(),
+			Header:   ft,
 		})
 		return nil
 	})
